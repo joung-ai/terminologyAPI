cc1746dfeeb8eb5c00e2869b3623a8d87c5a9326
joung-ai <yopkj12@gmail.com>
2026-01-29 17:01:16 +0800

    Initial Commit

---
+++ src/main/java/com/terminology/terminologyapi/web/RootController.java
@@ -0,0 +1,13 @@
+package com.terminology.terminologyapi.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RootController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Terminology API is running";
+    }
+}
